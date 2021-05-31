b10b008d8494ab02da7af9751cd7b01e3831ec76
AndrePetitPrince <81327209+AndrePetitPrince@users.noreply.github.com>
2021-05-31 21:14:37 +0200

    Delete def_params_geo_fiz.py

--- def_params_geo_fiz.py
+++
@@ -1,8 +0,0 @@
-def def_params_geo_fiz():
-    x_0=input("Podaj poczatek przedzialu: ")
-    x_R=input("Podaj koniec przedzialu: ")
-    twb_L=input("Podaj typ warunku brzegowego lewego (D/N): ")
-    twb_R=input("Podaj typ warunku brzegowego prawego (D/N): ")
-    wwb_L=input("Podaj wartosc warunku brzegowego lewego: ")
-    wwb_R=input("Podaj wartosc warunku brzegowego prawego: ")
-    return [x_0, x_R, twb_L, twb_R, wwb_L, wwb_R]
